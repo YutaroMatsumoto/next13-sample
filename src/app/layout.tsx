479afbd8f35d28f12185a27acd0044418f1e3ef7
YutaroMatsumoto <spurs03170629@gmail.com>
2023-02-19 10:53:32 +0900

    🔧 tailwind, twin.macroの環境づくり

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import StyledComponentsRegistry from 'src/app/lib/registry'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  )
+}
---
+++ src/components/atoms/button/Buttons.tsx
@@ -0,0 +1,42 @@
+import 'twin.macro'
+import { useState } from 'react'
+import { BaseButtonStyle, ButtonStyle } from './baseStyles'
+
+export type ButtonType = 'green' | 'black'
+
+export type ButtonProps = {
+  className?: string
+  type?: ButtonType
+  title: string
+  onClick: () => void
+}
+
+export const Button = ({
+  className,
+  type = 'green',
+  title,
+  onClick,
+}: ButtonProps) => {
+  const [isHover, setIsHover] = useState<boolean>(false)
+
+  return (
+    <button
+      css={[BaseButtonStyle, ButtonStyle[type].button({ isHover })]}
+      className={className}
+      onClick={onClick}
+      onMouseOver={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <span css={[ButtonStyle[type].title({ isHover })]}>{title}</span>
+    </button>
+  )
+}
+
+// DisableButtonはisHoverを利用しないので、他のボタンとは別に管理
+export const DisableButton = (title: string) => {
+  return (
+    <button tw="border-gray30 cursor-default" disabled={true}>
+      <span tw="text-gray30">{title}</span>
+    </button>
+  )
+}
