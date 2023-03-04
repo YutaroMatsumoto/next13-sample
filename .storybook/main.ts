7d56e388f5c7df1a04d1679378379ee25d26f754
YutaroMatsumoto <spurs03170629@gmail.com>
2023-03-04 10:57:00 +0900

    設定ファイルをtsファイルに変更

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,14 @@
 // NOTE: 参考 -> https://github.com/storybookjs/storybook/blob/next/code/frameworks/nextjs/README.md#supported-features
-export default {
+
+import type { StorybookConfig } from '@storybook/nextjs'
+
+const config: StorybookConfig = {
   stories: ['../src/components/**/*.stories.tsx'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
+  addons: ['@storybook/addon-controls', '@storybook/addon-docs'],
 }
+
+export default config
