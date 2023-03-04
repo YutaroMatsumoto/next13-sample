9dedbc7211f7835456dcfcfb1967e1dfe0b9ba98
YutaroMatsumoto <spurs03170629@gmail.com>
2023-03-04 10:58:05 +0900

    storiesファイルの修正

--- src/components/atoms/button/Button.stories.tsx
+++ src/components/atoms/button/Button.stories.tsx
@@ -1,32 +1,57 @@
-import type { Meta } from '@storybook/react'
-
+import type { Meta, StoryObj } from '@storybook/react'
 import { Button } from './Buttons'
-const meta: Meta<typeof Button> = {
+
+type ComponentType<P> = (props: P) => any
+
+type CSFType<M extends Meta<any>> = M extends {
+  component: ComponentType<infer P>
+  args: infer D
+}
+  ? { args: Omit<P, keyof D> } & { args: Partial<P> }
+  : never
+
+const meta = {
   title: 'Button',
   component: Button,
-  parameters: {
-    backgrounds: {
-      values: [
-        { name: 'red', value: '#f00' },
-        { name: 'green', value: '#0f0' },
-        { name: 'blue', value: '#00f' },
-      ],
-    },
-  },
-}
+  args: { title: 'Button' },
+  tags: ['autodocs'], // enable automatic documentation page
+} satisfies Meta<typeof Button>
 
-export const Green = {
+export default meta
+
+type MetaType = typeof meta
+
+type MyStory = CSFType<MetaType>
+type Story = StoryObj<typeof Button>
+
+export const Green: MyStory = {
   args: {
     title: 'Button',
     onClick: () => console.warn('click'),
   },
 }
+console.log(Green)
 
-export const Black = {
+export const Black: Story = {
   args: {
     ...Green.args,
     type: 'black',
   },
 }
 
-export default meta
+// type Colors = 'red' | 'green' | 'blue'
+
+// type RGB = [red: number, green: number, blue: number]
+
+// const palette = {
+//   red: [255, 0, 0],
+//   green: '#00ff00',
+//   blue: [0, 0, 255],
+//   //  ~~~~ The typo is now correctly detected
+// } satisfies Record<Colors, string | RGB>
+
+// // 'red'には配列メソッドを使えるように使いたい
+// const redComponent = palette.red.at(0)
+
+// // また'green'には文字列メソッドを使えるように使いたい
+// const greenNormalized = palette.green.toUpperCase()
---
+++ src/components/atoms/button/Test.stories.tsx
@@ -0,0 +1,23 @@
+/**
+ * CSF2 の書き方
+ */
+
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Button } from './Buttons'
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'CSF2',
+  component: Button,
+  args: {
+    title: 'CSF2',
+  },
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+
+export const GreenCSF2 = Template.bind({})
