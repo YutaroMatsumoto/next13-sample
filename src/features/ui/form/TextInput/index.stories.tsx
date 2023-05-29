3415193bbe29d04ed3bfe3320c995e3547852e28
松本　悠太郎 <yutaro.matsumoto@PC-4421.local>
2023-05-29 20:27:41 +0900

    TextInputのstory作成

--- src/features/ui/form/TextInput/index.stories.tsx
+++ src/features/ui/form/TextInput/index.stories.tsx
@@ -1,14 +1,25 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { TextInput } from '.'
+import { useForm } from 'react-hook-form'
 const meta = {
-  title: 'Button',
+  title: 'TextInput',
   component: TextInput,
-  args: { title: 'TextInput' },
+  args: {},
   argTypes: {
-    onClick: { action: 'clicked' },
     className: { table: { disable: true } }, // これでcontrolsから対象外にできる
+    registeration: { table: { disable: true } }, // これでcontrolsから対象外にできる
   },
   tags: ['autodocs'], // enable automatic documentation page
 } satisfies Meta<typeof TextInput>
 
 export default meta
+
+const TextInputWithHooks = () => {
+  const { register } = useForm()
+  return <TextInput registeration={register('sample')} />
+}
+
+type Story = StoryObj<typeof meta>
+export const Default: Story = {
+  render: () => <TextInputWithHooks />,
+}
--- src/features/ui/form/TextInput/index.tsx
+++ src/features/ui/form/TextInput/index.tsx
@@ -1,46 +1,23 @@
-import { forwardRef } from 'react'
-import { UseFormRegisterReturn } from 'react-hook-form'
 import 'twin.macro'
+import { UseFormRegisterReturn } from 'react-hook-form'
 
 type Props = {
-  registeration: UseFormRegisterReturn
+  registeration?: UseFormRegisterReturn
   className?: string
 }
 
-// type Props = React.ComponentPropsWithRef<'input'> & {
-//   registeration: UseFormRegisterReturn
-//   className?: string
-// }
-
-// fprwardrefを使ってみたパターン。だが、不要？
-// export const TextInput = forwardRef<HTMLInputElement, Props>(function TextInput(
-//   { registeration, className },
-//   ref
-// ) {
-//   return (
-//     <input
-//       tw="w-full border-solid border border-gray30 rounded-lg px-2 py-1 focus:outline-none focus:border focus:border-green40"
-//       className={className}
-//       // ref={ref}
-//       {...registeration}
-//     />
-//   )
-// })
-
-// この場合、ref属性を親から受け取っているわけではないのでforwardRefの使用は必要ない？
 export const TextInput = ({ registeration, className }: Props) => {
   return (
     <input
       type="text"
       tw="w-full border-solid border border-gray30 rounded-lg px-2 py-1 focus:outline-none focus:border focus:border-green40"
       className={className}
-      // ref={ref}
       {...registeration}
     />
   )
 }
 
-// 今回のように非制御コンポーネントで利用する場合、以下を参考にして、ref属性を受け取る必要がある？
+// 今回のように非制御コンポーネントで利用する場合、以下を参考にして、ref属性を受け取る書き方もできる？
 // import clsx from "clsx";
 // import { forwardRef } from "react";
 // import styles from "./styles.module.css";
