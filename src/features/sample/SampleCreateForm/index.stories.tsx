d929e6c4be9288f3258df1ed3d0e33c65123b1ac
松本　悠太郎 <yutaro.matsumoto@PC-4421.local>
2023-06-06 19:58:13 +0900

    play functionをコメントアウトしてみた

--- src/features/sample/SampleCreateForm/index.stories.tsx
+++ src/features/sample/SampleCreateForm/index.stories.tsx
@@ -14,39 +14,37 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Succeed: Story = {
-  play: async ({ canvasElement, step }) => {
-    const canvas = within(canvasElement)
-
-    await step('テキストボックスに入力', async () => {
-      const titleTextbox = canvas.getByRole('textbox', {
-        name: 'タイトル',
-      })
-      await userEvent.type(titleTextbox, 'タイトル')
-    })
-
-    await step('送信ボタンをクリックして、送信成功すること', async () => {
-      const submitButton = canvas.getByRole('button', { name: '送信' })
-      await userEvent.click(submitButton)
-      await waitFor(() => {
-        // const successMessage = canvas.getByText('送信成功！')
-        // expect(canvas.getByText('送信成功！')).toBeInTheDocument()
-      })
-    })
-  },
+  // play: async ({ canvasElement, step }) => {
+  //   const canvas = within(canvasElement)
+  //   await step('テキストボックスに入力', async () => {
+  //     const titleTextbox = canvas.getByRole('textbox', {
+  //       name: 'タイトル',
+  //     })
+  //     await userEvent.type(titleTextbox, 'タイトル')
+  //   })
+  //   await step('送信ボタンをクリックして、送信成功すること', async () => {
+  //     const submitButton = canvas.getByRole('button', { name: '送信' })
+  //     await userEvent.click(submitButton)
+  //     await waitFor(() => {
+  //       // const successMessage = canvas.getByText('送信成功！')
+  //       // expect(canvas.getByText('送信成功！')).toBeInTheDocument()
+  //     })
+  //   })
+  // },
 }
 
 export const Failed: Story = {
-  play: async ({ canvasElement, step }) => {
-    const canvas = within(canvasElement)
-    await step('送信ボタンをクリック', async () => {
-      const titleTextbox = canvas.getByRole('textbox', {
-        name: 'タイトル',
-      })
-      const submitButton = canvas.getByRole('button', { name: '送信' })
-      await userEvent.click(submitButton)
-      await waitFor(() => {
-        expect(titleTextbox).toHaveErrorMessage('必須です')
-      })
-    })
-  },
+  // play: async ({ canvasElement, step }) => {
+  //   const canvas = within(canvasElement)
+  //   await step('送信ボタンをクリック', async () => {
+  //     const titleTextbox = canvas.getByRole('textbox', {
+  //       name: 'タイトル',
+  //     })
+  //     const submitButton = canvas.getByRole('button', { name: '送信' })
+  //     await userEvent.click(submitButton)
+  //     await waitFor(() => {
+  //       expect(titleTextbox).toHaveErrorMessage('必須です')
+  //     })
+  //   })
+  // },
 }
